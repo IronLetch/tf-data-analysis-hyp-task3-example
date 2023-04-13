5bf2ec92c419660c518cf470561ac84ec44be944
IronLetch <78549098+IronLetch@users.noreply.github.com>
2023-04-14 00:08:51 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,19 +1,10 @@
 import pandas as pd
 import numpy as np
 
-
-from statsmodels.stats.proportion import proportions_ztest
+import statsmodels.stats.weightstats as w
 
 chat_id = 531503618 # Ваш chat ID, не меняйте название переменной
 
-def solution(x_success: int, x_cnt: int, y_success: int, y_cnt: int) -> bool:
-
-    alpha = 0.04
-
-    _, p_value = proportions_ztest(
-
-        [x_success, y_success], [x_cnt, y_cnt], alternative="smaller"
-
-    )
-
-    return p_value <= alpha
+def solution(x: np.array, y: np.array) -> bool:  # Одна или две выборке на входе, заполняется исходя из условия
+    _, pvalue = w.ztest(x, y, value=500, alternative='larger')
+    return pvalue <= 0.04
